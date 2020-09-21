7d6bdf86657dc482e407d375af2461dbbaddfeba
boom100100 <bsd231@nyu.edu>
2020-09-21 16:25:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,103 +1,96 @@
-function largestSubarraySum(array){
-  let i = 0;
-  let j = i;
-
-  let largest;// = array[0];
-  let negatives = array.filter(x => x < 0);
-  const summer = (accumulator, current) => accumulator + current;
-
-  if (array.length === negatives.length)
-    return Math.max(...negatives);
-  if (negatives.length === 0)
-    return array.reduce(summer);
-
-  while (i < array.length){
-    const result = array.slice(i, j + 1).reduce(summer);
-
-    if (result > largest || typeof largest === 'undefined')
-      largest = result;
-
-    j++;
-    if (j === array.length){
-      i++;
-      j = i;
-    }
-  }
-  return largest;
-}
-
 // function largestSubarraySum(array){
-//   // array indexes
 //   let i = 0;
-//   let j = array.length - 1;
-//   let negatives = array.filter(x => x < 0);
-//   // groups indexes
-//   let k = 0;
-//   let l = 0;
+//   let j = i;
 //
-//   let groups = [[]];
-//   let groups2 = [[]];
+//   let largest;// = array[0];
+//   let negatives = array.filter(x => x < 0);
+//   const summer = (accumulator, current) => accumulator + current;
 //
 //   if (array.length === negatives.length)
 //     return Math.max(...negatives);
+//   if (negatives.length === 0)
+//     return array.reduce(summer);
 //
-//   while (true){
-//     if (array[0] < 0)
-//       array.shift();
-//     if (array[array.length - 1] < 0)
-//       array.pop();
-//     if ((array[0] > 0 && array[array.length - 1] > 0) || array.length === 0)
-//       break;
-//   }
-//   while (i < array.length || j >= 0){
-//     let value1 = array[i];
-//     let value2 = array[j];
-//     // 1,2,-1,3
+//   while (i < array.length){
+//     const result = array.slice(i, j + 1).reduce(summer);
 //
-//     if (value1 > 0) {
-//       groups[k].push(value1);
+//     if (result > largest || typeof largest === 'undefined')
+//       largest = result;
+//
+//     j++;
+//     if (j === array.length){
 //       i++;
-//     } else if (value1 < 0){
-//
-//       groups.push([...groups[k]]);
-//       k++;
-//       i++;
-//
-//       groups[k].push(value1);
-//
-//     }
-//
-//     if (value2 > 0) {
-//       groups2[l].push(value2);
-//       j--;
-//     } else if (value2 < 0){
-//       // if (j !== array.length - 1){
-//         groups2.push([...groups2[l]]);
-//         j--;
-//         l++;
-//         groups2[l].push(value2);
-//       // }
+//       j = i;
 //     }
 //   }
-//
-//   console.log('groups', groups);
-//   console.log('groups2', groups2);
-//   const sum = (a, c) => a + c;
-//   const sums = [];
-//
-//   getSums(groups, sum, sums);
-//   getSums(groups2, sum, sums);
-//
-//   return Math.max(...sums);
+//   return largest;
 // }
-//
-// function getSums(groups, sum, sums){
-//   for (let group of groups){
-//     if (group.length === 0)
-//       continue;
-//
-//     let result = group.reduce(sum);
-//     sums.push(result);
-//
-//   }
-// }
+
+function largestSubarraySum(array){
+  // array indexes
+  let i = 0;
+  let j = array.length - 1;
+  let negatives = array.filter(x => x < 0);
+  // groups indexes
+  let k = 0;
+  let l = 0;
+
+  let groups = [[]];
+  let groups2 = [[]];
+
+  if (array.length === negatives.length)
+    return Math.max(...negatives);
+
+  while (true){
+    if (array[0] < 0)
+      array.shift();
+    if (array[array.length - 1] < 0)
+      array.pop();
+    if ((array[0] >= 0 && array[array.length - 1] >= 0) || array.length === 0)
+      break;
+  }
+  while (i < array.length || j > -1){
+    let value1 = array[i];
+    let value2 = array[j];
+    // 1,2,-1,3
+
+    if (value1 > 0) {
+      groups[k].push(value1);
+    } else if (value1 < 0){
+      groups.push([...groups[k]]);
+      k++;
+      groups[k].push(value1);
+    }
+    i++;
+
+    if (value2 > 0) {
+      groups2[l].push(value2);
+    } else if (value2 < 0){
+        groups2.push([...groups2[l]]);
+        l++;
+        groups2[l].push(value2);
+    }
+    j--;
+  }
+
+  // console.log('groups', groups);
+  // console.log('groups2', groups2);
+  const sum = (a, c) => a + c;
+  const sums = [];
+
+  getSums(groups, sum, sums);
+  getSums(groups2, sum, sums);
+
+  return Math.max(...sums);
+}
+
+function getSums(groups, sum, sums){
+  for (let group of groups){
+    if (group.length === 0)
+      continue;
+
+    let result = group.reduce(sum);
+    sums.push(result);
+
+  }
+}
